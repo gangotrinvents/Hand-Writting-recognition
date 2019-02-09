d5ad87cfa27d8b74717c15d81de30865a4e83dc7
gangotrinvents <46564926+gangotricertification@users.noreply.github.com>
2019-02-09 23:30:22 +0530

    Add files via upload

---
+++ hand written integer recogniton/handwriting recognition letters.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+ 
+digits = cv2.imread("digits.png", cv2.IMREAD_GRAYSCALE)
+test_digits = cv2.imread("test_digits.png", cv2.IMREAD_GRAYSCALE)
+ 
+rows = np.vsplit(digits, 50)
+cells = []
+for row in rows:
+    row_cells = np.hsplit(row, 50)
+    for cell in row_cells:
+        cell = cell.flatten()
+        cells.append(cell)
+cells = np.array(cells, dtype=np.float32)
+ 
+k = np.arange(10)
+cells_labels = np.repeat(k, 250)
+ 
+ 
+test_digits = np.vsplit(test_digits, 50)
+test_cells = []
+for d in test_digits:
+    d = d.flatten()
+    test_cells.append(d)
+test_cells = np.array(test_cells, dtype=np.float32)
+ 
+ 
+# KNN
+knn = cv2.ml.KNearest_create()
+knn.train(cells, cv2.ml.ROW_SAMPLE, cells_labels)
+ret, result, neighbours, dist = knn.findNearest(test_cells, k=3)
+ 
+ 
+print(result)
